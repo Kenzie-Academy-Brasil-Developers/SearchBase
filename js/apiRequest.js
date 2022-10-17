e700b1b4ed9983c85af13ea2afd3840fbbd64049
Mateus9286 <106389123+Mateus9286@users.noreply.github.com>
2022-10-17 13:07:19 -0300

    Entrega

--- js/apiRequest.js
+++ js/apiRequest.js
@@ -13,7 +13,7 @@ async function request() {
     .then(async (res) => {
       localStorage.setItem("nome", res.login || "");
       localStorage.setItem("proficao", res.bio  || "");
-      localStorage.setItem("E-mail", res.email  || "");
+      localStorage.setItem("E-mail",`mailto:${res.email}` || "");
       localStorage.setItem("image", res.avatar_url || "");
       localStorage.setItem("idUser", res.id || "");
       localStorage.setItem("linkPerfil", res.html_url || "");
